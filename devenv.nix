8fc05579227f5e3ec25c47951833af1c06684751
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2025-02-21 04:29:32 +1100

    setup difftastic

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  # packages = [ pkgs.git ];
+  packages = [ pkgs.difftastic ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
